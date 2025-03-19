1a274e3e4b2ceadcbd4a9d95343ca57e9efbedce
Zahidsherkhan <myselene000@gmail.com>
2025-03-19 16:17:11 +0500

    ChatUI Updated and Hero Text Enlarged

--- src/Pages/Home/components/subComponents/AfterHero_SUBS/Chat.jsx
+++ src/Pages/Home/components/subComponents/AfterHero_SUBS/Chat.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import styling from "./subComponentsStyling/Chat.module.css";
 import profilePic from "./chatPerson.jpg";
+import { MdSend } from "react-icons/md";
 const Chat = () => {
   return (
-    <div className={styling.ChatParent}>
-      <div className={styling.picAndName}>
-        <img src={profilePic} alt="" className={styling.profilePic}/>
-        <p className={profilePic.name}>Touseef From Naxon Solution</p>
+    <>
+      <div className={styling.ChatParent}>
+        <div className={styling.picAndName}>
+          <img src={profilePic} alt="" className={styling.profilePic} />
+          <p className={profilePic.name}>Touseef From Naxon Solution</p>
+        </div>
+        <p className={styling.question}>
+          Cheers! Thank You for Choosing Our Services. How can we help you?
+        </p>
+        <input className={styling.input} type="text" placeholder="Name" />
+        <input className={styling.input} type="text" placeholder="Email" />
+        <input
+          className={styling.input}
+          type="text"
+          placeholder="Write Your Message"
+        />
+        <button className={styling.sendBtn}>
+          <MdSend />
+        </button>
       </div>
-      <p className={styling.question}>Cheers! Thank You for Choosing Our Services. How can we help you?</p>
-      <input className={styling.input} type="text" placeholder="Write Your Message" />
-    </div>
+    </>
   );
 };
 
