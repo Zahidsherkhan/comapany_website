afb5b9e2ddf40141dee67d70d7efdae7bf7c57f7
Zahidsherkhan <myselene000@gmail.com>
2025-03-25 11:22:08 +0500

    Chat Responsiveness and Text Updated

--- src/Pages/Home/components/subComponents/AfterHero_SUBS/Chat.jsx
+++ src/Pages/Home/components/subComponents/AfterHero_SUBS/Chat.jsx
@@ -8,10 +8,10 @@ const Chat = () => {
       <div className={styling.ChatParent}>
         <div className={styling.picAndName}>
           <img src={profilePic} alt="" className={styling.profilePic} />
-          <p className={profilePic.name}>Touseef From Naxon Solution</p>
+          <p className={profilePic.name}>Contact Naxon Solution</p>
         </div>
         <p className={styling.question}>
-          Cheers! Thank You for Choosing Our Services. How can we help you?
+          Thank You for Choosing Our Services. How can we help you?
         </p>
 
         <form action="submit">
